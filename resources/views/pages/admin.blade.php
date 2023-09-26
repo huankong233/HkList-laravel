5b9837b1d899d004ae2c0b0c008b991159553d7f
huan_kong <2564076459@qq.com>
2023-09-26 10:44:31 +0800

    按钮文字

--- resources/views/pages/admin.blade.php
+++ resources/views/pages/admin.blade.php
@@ -214,7 +214,9 @@
             <el-tab-pane label="退出登陆" name="logout">
                 <el-card>
                     <el-button type="primary"
-                               @click="logout"></el-button>
+                               @click="logout">
+                        退出登陆
+                    </el-button>
                 </el-card>
             </el-tab-pane>
             <el-tab-pane label="开源说明" name="openSourceNotice">
