bf9b7a41d4f473e3ac6b75ec68f71264d1afd7d1
huan_kong <2564076459@qq.com>
2023-10-10 15:11:34 +0800

    重构前端部分

--- resources/views/layouts/main.blade.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_','-',strtolower(app()->getLocale())) }}">
-@include('layouts.head')
-<body>
-<div id="app">
-    @yield('template')
-</div>
-@yield('scripts')
-</body>
---
+++ resources/views/main.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"/>
+    <link rel="icon" href="/favicon.ico"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta name="app_name" content="{{config("app.name")}}"/>
+    <title>{{config("app.name")}}</title>
+    <script type="module" crossorigin src="/assets/index-1401672a.js"></script>
+    <link rel="stylesheet" href="/assets/index-e965689e.css">
+</head>
+<body>
+<div id="app"></div>
+
+</body>
+</html>
