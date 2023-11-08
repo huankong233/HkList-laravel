62a86418eebedb867123076f6b7694edc297c0b3
huan_kong <2564076459@qq.com>
2023-11-08 10:27:53 +0800

    更新入口文件

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -24,12 +24,7 @@ fi && \
 # 判断环境变量APP_AUTO_UPDATE的值
 if [ "$APP_AUTO_UPDATE" = "true" ]; then
     # 值为true时
-<<<<<<< HEAD
     php artisan app:check-app-status
-=======
-    python3 /python/check_update.py
-    # 等待运行结束后再执行底部的exec "$@"
->>>>>>> 1524e35d18730ee217f65a17d70307ccbe9309af
 elif [ "$APP_AUTO_UPDATE" = "false" ]; then
     # 值为false时
     echo "没有开启更新检测…"
