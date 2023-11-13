1ef6055d3a5e49f5287085d3d398801c3dd88db3
Aqr-K <95741669+Aqr-K@users.noreply.github.com>
2023-11-13 14:22:46 +0800

    修复错误的参数
    增加部分文本输出

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-# 目录映射的变量
+# 变量
 dir_path="/var/www/html"
 latest_path="/var/www/94list-laravel"
+commands_path="/app/Console/Commands"
 
 # 目录映射检测
 if [ -d "$dir_path" ]; then
@@ -10,34 +11,36 @@ if [ -d "$dir_path" ]; then
     # 判断文件夹是否为空
     if [ ! "$(ls -A $dir_path)" ]; then
         # 文件夹为空 复制文件夹内容
-        cp -a "$latest_path" "$dir_path"
+        cp -a "$latest_path"/. "$dir_path"
     fi
 else
     # 文件夹不存在
     echo "没有正确映射路径…"
-fi
+fi && \
 
 # 兼容老版本不存在 复制自定义命令文件夹
-commands_path=$dir_path/app/Console/Commands
-if [ -d "$commands_path" ]; then
-    echo "检测到已存在Commands文件夹,开始更新"
-    rm -rf $commands_path
+
+if [ -d "$dir_path""$commands_path" ]; then
+    echo "检测到已存在Commands文件夹,开始更新…" && \
+    rm -rf "$dir_path""$commands_path"/*
 else
-    echo "未检测到Commands文件夹,开始重新插入"
-    mkdir $commands_path
-fi
+    echo "未检测到Commands文件夹,开始重新插入…" && \
+    mkdir -p "$dir_path""$commands_path"
+fi && \
 
 # 复制文件夹内容
-cp -r "$latest_path/app/Console/Commands/*" $commands_path
+echo "导入Commands文件夹…" && \
+cp -a "$latest_path""$commands_path"/. "$dir_path""$commands_path" && \
 
 # 复制 vendor 文件夹
-rm -rf "$dir_path/vendor"
-cp -r "$latest_path/vendor" "$dir_path/vendor"
+echo "导入新的依赖…" && \
+rm -rf "$dir_path"/vendor && \
+cp -a "$latest_path"/vendor "$dir_path"/vendor && \
 
 # 判断环境变量APP_AUTO_UPDATE的值
 if [ "$APP_AUTO_UPDATE" = "true" ]; then
     # 值为true时
-    cd $latest_path
+    cd $latest_path 
     php artisan app:check-app-status
     elif [ "$APP_AUTO_UPDATE" = "false" ]; then
     # 值为false时
