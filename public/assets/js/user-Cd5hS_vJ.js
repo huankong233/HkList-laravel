642e1282ca7227a3a848695d17db3f061e5b7793
huan_kong <2564076459@qq.com>
2024-05-10 17:14:22 +0800

    [*] 更新前端

---
+++ public/assets/js/user-Cd5hS_vJ.js
@@ -0,0 +1,1 @@
+import{aa as e}from"./index-niWMSR8p.js";const t=s=>e.post("/user/login",s),r=s=>e.post("/user/register",s),a=()=>e.delete("/user");export{a,t as l,r};
--- public/assets/js/user-DFWkiVqt.js
+++
@@ -1,1 +0,0 @@
-import{aa as e}from"./index-q5nPSmrO.js";const t=s=>e.post("/user/login",s),r=s=>e.post("/user/register",s),a=()=>e.delete("/user");export{a,t as l,r};
