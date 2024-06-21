50cc57ad648972c049054dc34f653058702ac52c
huan_kong <huank0ng@foxmail.com>
2024-06-21 14:25:05 +0800

    [*] 修复 `Aria2` 下载链接错误的问题

--- public/assets/js/aria2Store-DYHNTz5R.js
+++
@@ -1,1 +0,0 @@
-import{j as o,k as s}from"./index-CDwOllFt.js";import{d as n,r as e,E as t}from"./.pnpm-BPJ2h7HZ.js";const g=n("aria2Store",()=>{const i=e(!1),r=e(o()),a=e(null);return{aria2ConfigDialogVisible:i,aria2ConfigForm:r,aria2ConfigFormRef:a,saveAria2Config:async()=>{!a.value||!await a.value.validate()||(s(r.value),t.success("保存成功"),i.value=!1)}}});export{g as u};
