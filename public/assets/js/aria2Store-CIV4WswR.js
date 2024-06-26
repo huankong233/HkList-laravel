ba2bb9ef90573198d86e639427ef8a773c843ae5
huan_kong <huank0ng@foxmail.com>
2024-06-26 19:46:02 +0800

    [*] 更新前端

--- public/assets/js/aria2Store-CIV4WswR.js
+++
@@ -1,1 +0,0 @@
-import{k as o,m as s}from"./index-CG8uS_64.js";import{d as n,r as e,E as t}from"./.pnpm-CR973naf.js";const g=n("aria2Store",()=>{const i=e(!1),r=e(o()),a=e(null);return{aria2ConfigDialogVisible:i,aria2ConfigForm:r,aria2ConfigFormRef:a,saveAria2Config:async()=>{!a.value||!await a.value.validate()||(s(r.value),t.success("保存成功"),i.value=!1)}}});export{g as u};
